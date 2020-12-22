9ca59cbd0e0b7690ecae10f0e42d47b6f80aa6b0
math-yew <math.u@hotmail.com>
2020-12-22 12:11:40 -0700

    basic website connected

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
 export let content = {
-  "title": "chicken",
-  "description": "fries"
+  "title": "My Website",
+  "subTitle": "That's right",
+  "subject1": "All about it",
+  "words1": "This website is a static website.  But I'm making it so that you can change it without using code.  Instead you use a local website on your computer to change it.",
+  "copywrite": "2020"
 }
--- contentModule.js
+++ contentModule.js
@@ -1,7 +1,6 @@
-console.log("content module");
-
 import { content } from "./content.js";
 
 console.log(content.title);
-
-$("#test").text(content.title);
+for(let key in content){
+  $("#"+key).text(content[key]);
+}
